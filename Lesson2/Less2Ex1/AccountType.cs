39d7e02a4f9aee137150a0c4a4290a3e5fc753dd
VladimirDolguev <avus93@mail.ru>
2021-10-11 19:17:02 +0300

    Урок 2. Часть 1
    1. Создать класс счет в банке с закрытыми полями: номер счета, баланс, тип банковского счета (использовать перечислимый тип). Предусмотреть методы для доступа к данным – заполнения и чтения. Создать объект класса, заполнить его поля и вывести информацию об объекте класса на печать.

---
+++ Lesson2/Less2Ex1/AccountType.cs
@@ -0,0 +1,9 @@
+﻿namespace Less2Ex1
+{
+    public enum AccountType
+    {
+        Deposit,
+        Savings,
+        MoneyMarket
+    }
+}
---
+++ Lesson2/Less2Ex1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Less2Ex1
+{
+    /*
+        Долгуев Владимир.
+        1. Создать класс счет в банке с закрытыми полями: номер счета, баланс, тип банковского счета (использовать перечислимый тип).
+        Предусмотреть методы для доступа к данным – заполнения и чтения.
+        Создать объект класса, заполнить его поля и вывести информацию об объекте класса на печать.
+     */
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var account = new BankAccount
+            {
+                Id = 1,
+                Balance = 1000,
+                AccountType = AccountType.Deposit
+            };
+
+            Console.WriteLine(account);
+
+            Console.ReadKey();
+        }
+    }
+}
