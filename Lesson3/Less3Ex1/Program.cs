2dc7e6a166f1bcec0d0d31e79b4195b1097a48f3
VladimirDolguev <avus93@mail.ru>
2021-10-20 21:36:21 +0300

    Урок 3 Задание 1
    1. В класс банковский счет, созданный в упражнениях, добавить метод, который переводит деньги с одного счета на другой. У метода два параметра: ссылка на объект класса банковский счет откуда снимаются деньги, второй параметр – сумма.

---
+++ Lesson3/Less3Ex1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Less3Ex1
+{
+    /*
+        Долгуев Владимир.
+        1. В класс банковский счет, созданный в упражнениях, добавить метод, который переводит деньги с одного счета на другой.
+        У метода два параметра: ссылка на объект класса банковский счет, откуда снимаются деньги, второй параметр – сумма.
+     */
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var account = new BankAccount(1000, AccountType.Savings);
+            Console.WriteLine(account);
+
+            var newAccount = new BankAccount(1000, AccountType.Savings);
+            Console.WriteLine(newAccount);
+
+            account.TransferFrom(ref newAccount, 500);
+            Console.WriteLine($"Переводим 500 рублей со счета {newAccount.Id} на счет {account.Id}.");
+            Console.WriteLine(account);
+            Console.WriteLine(newAccount);
+
+            Console.ReadKey();
+        }
+    }
+}
